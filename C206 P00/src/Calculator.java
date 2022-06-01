4dd820e9c9c318c2a9a57a57d29b66baa2131b54
21023096 <21023096@DESKTOP-CHNVKED>
2022-06-01 14:42:15 +0800

    first push to P00 repository in GitHbu

---
+++ C206 P00/src/Calculator.java
@@ -0,0 +1,28 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21023096, 1 Jun 2022 11:12:30 am
+ */
+
+public class Calculator {
+	
+	public int add (int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract (int a, int b) {
+		return a - b;
+	}
+	
+	public int multiply (int a, int b) {
+		return a * b;
+	}
+	
+	public int divide (int a, int b) {
+		return a/b;
+	}
+
+}
---
+++ C206 P00/src/CalculatorTest.java
@@ -0,0 +1,91 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21023096, 1 Jun 2022 11:17:30 am
+ */
+
+public class CalculatorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		
+		//Arrange
+		int a = 4321;
+		int b = 1234;
+		Calculator cal = new Calculator();
+		
+		//Act
+		int actual = cal.add(a, b);
+		
+		//Assert
+		int expected = 5555;
+		assertEquals(expected,actual);
+	
+	}
+	
+	@Test
+	public void testSubtract() {
+		
+		//Arrange
+		int a = 9876;
+		int b = 4321;
+		Calculator cal = new Calculator();
+		
+		//Act
+		int actual = cal.subtract(a,b);
+		
+		//Assert
+		int expected = 5555;
+		assertEquals(expected,actual);
+	}
+	
+	@Test
+	public void testMultiply() {
+		
+		//Arrange
+		int a = 1;
+		int b = 5;
+		Calculator cal = new Calculator();
+		
+		//Act
+		int actual = cal.multiply(a,b);
+		
+		//Assert
+		int expected = 5;
+		assertEquals(expected,actual);
+	}
+	
+	@Test
+	public void testDivide() {
+		
+		//Arrange
+		int a = 15;
+		int b = 3;
+		Calculator cal = new Calculator();
+		
+		//Act
+		int actual = cal.divide(a,b);
+		
+		//Assert
+		int expected = 5;
+		assertEquals(expected,actual);
+	}
+	
+
+}
